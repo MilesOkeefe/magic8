7d5574c068905570157f569a06294d64ecf87c48
Miles O'Keefe <miles.okeefe@gmail.com>
2014-01-07 12:30:38 -0500

    Migrate to firmware v2.0, message changes on shake, background is black with white text

--- src/magic8.c
+++ src/magic8.c
@@ -1,20 +1,11 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
+#include <time.h>
 
-#define MY_UUID { 0x46, 0xCB, 0x20, 0xBD, 0x22, 0xD4, 0x47, 0xA6, 0xA6, 0x7E, 0x9B, 0x31, 0x34, 0x4E, 0xAA, 0x74 }
-PBL_APP_INFO(MY_UUID,
-             "Magic Eightball", "Gergely Imreh",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
-
-Window window;
-
-Layer layer;
+static Window *window;
+static TextLayer *text_layer;
 
 /* Possible answers */
-char *answers[20] = {"It is certain",
+static char *answers[20] = {"It is certain",
 		     "It is decidedly so",
 		     "Without a doubt",
 		     "Yes definitely",
@@ -37,52 +28,58 @@ char *answers[20] = {"It is certain",
 
 /* random() related parts are from https://github.com/ad1217/pebbloids/blob/master/src/Pebbloids.c */
 long seed;
-int random(int max){
+static int random(int max){
     seed=(((seed*214013L+2531011L) >> 16) & 32767);
     return ((seed%max));
 }
 
-void update_layer_callback(Layer *me, GContext* ctx) {
-  (void)me;
-  int reading = random(20);
+void update_message(void){
+	int reading = random(20);
+	text_layer_set_text(text_layer, answers[reading]);
+}
+void accel_tap_handler(AccelAxisType axis, int32_t direction){
+  update_message();
+}
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
 
-  graphics_context_set_text_color(ctx, GColorBlack);
-
-  graphics_text_draw(ctx,
-		     answers[reading],
-		     /* fonts_get_system_font(FONT_KEY_FONT_FALLBACK), */
-		     fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD),
-		     GRect(10, 10, 144-15, 100),
-		     GTextOverflowModeWordWrap,
-		     GTextAlignmentLeft,
-		     NULL);
-
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, bounds.size.h } });
+  window_set_background_color(window, GColorBlack);
+  window_set_fullscreen(window, true);
+  text_layer_set_background_color(text_layer, GColorBlack);
+  text_layer_set_text_color(text_layer, GColorWhite);
+  update_message();
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
 }
 
+static void init(void) {
+	window = window_create();
+	window_set_window_handlers(window, (WindowHandlers) {
+	    .load = window_load,
+	    .unload = window_unload,
+	  });
+	const bool animated = true;
+	window_stack_push(window, animated);
 
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
-
-  window_init(&window, "Magic 8-ball");
-  window_stack_push(&window, true);
-
-
-  layer_init(&layer, window.layer.frame);
-  layer.update_proc = update_layer_callback;
-  layer_add_child(&window.layer, &layer);
-
-  layer_mark_dirty(&layer);
-
-  /* Random seed */
-  PblTm time;
-  get_time(&time);
-  seed=time.tm_hour*3600+time.tm_min*60+time.tm_sec;
+	/* Random seed */
+	time_t timval=time(NULL);
+	struct tm tims=*(localtime(&timval));
+	seed=tims.tm_hour*3600+tims.tm_min*60+tims.tm_sec;
+	accel_tap_service_subscribe(&accel_tap_handler);
+}
+static void deinit(void) {
+	accel_tap_service_unsubscribe();
+	window_destroy(window);
 }
 
-
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
-  };
-  app_event_loop(params, &handlers);
+int main(void) {
+	init();
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+	app_event_loop();
+	deinit();
 }
