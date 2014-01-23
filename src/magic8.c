480c087b34f3e672b842b7f0bf63245b5c2d03df
Miles O'Keefe <miles.okeefe@gmail.com>
2014-01-22 19:28:01 -0600

    Added icon, changed font, light is always on, text changes to simulate ball

--- src/magic8.c
+++ src/magic8.c
@@ -3,6 +3,8 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static TextLayer *black_text_layer;
+static AppTimer *timer;
 
 /* Possible answers */
 static char *answers[20] = {"It is certain",
@@ -32,10 +34,36 @@ static int random(int max){
     seed=(((seed*214013L+2531011L) >> 16) & 32767);
     return ((seed%max));
 }
+int count;
+int max;
+int reading;
+static void timer_callback(void *data) {
+	if(count < max){
+		if(count%2 == 1){
+			reading = random(20);
+			text_layer_set_text(text_layer, answers[reading]);
+		}else{
+			text_layer_set_text(text_layer, "--------");
+		}
+		timer = app_timer_register(100 /* milliseconds */, timer_callback, NULL);
+		count++;
+	}else{
+		reading = random(20);
+		text_layer_set_text(text_layer, answers[reading]);
+	}
+}
 
 void update_message(void){
-	int reading = random(20);
-	text_layer_set_text(text_layer, answers[reading]);
+	count = 0;
+	max = random(5) + 2;
+	timer = app_timer_register(300 /* milliseconds */, timer_callback, NULL);
+	light_enable(true);//backlight on black background looks blue like a magic 8 ball
+}
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  update_message();
+}
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 void accel_tap_handler(AccelAxisType axis, int32_t direction){
   update_message();
@@ -44,11 +72,13 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, bounds.size.h } });
+  text_layer = text_layer_create((GRect) { .origin = { 0, 70 }, .size = { bounds.size.w , bounds.size.h} });
+  text_layer_set_background_color(text_layer, GColorBlack);
   window_set_background_color(window, GColorBlack);
-  window_set_fullscreen(window, true);
+  window_set_background_color(window, GColorBlack);
   text_layer_set_background_color(text_layer, GColorBlack);
   text_layer_set_text_color(text_layer, GColorWhite);
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   update_message();
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
@@ -59,11 +89,13 @@ static void window_unload(Window *window) {
 
 static void init(void) {
 	window = window_create();
+	window_set_click_config_provider(window, click_config_provider);
 	window_set_window_handlers(window, (WindowHandlers) {
 	    .load = window_load,
-	    .unload = window_unload,
+	    .unload = window_unload
 	  });
 	const bool animated = true;
+    window_set_fullscreen(window, true);
 	window_stack_push(window, animated);
 
 	/* Random seed */
